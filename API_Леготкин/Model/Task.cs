4002a71c1279dc7789758dcaf3aa77912685cc76
4epni4ka <legotkinc2006@gmail.com>
2024-05-29 06:09:55 +0500

    Разработанные модели данных. Леготкин

---
+++ API_Леготкин/Model/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace API_Леготкин.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Priority { get; set; }
+        public DateTime DataExecute { get; set; }
+        public string Comment { get; set; }
+        public bool Done { get; set; }
+    }
+}
