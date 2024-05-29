22fcbc21ea004cf68476029a99d923ef1cd718a2
4epni4ka <legotkinc2006@gmail.com>
2024-05-29 13:10:16 +0500

    Создание модели и контекста пользователя. Леготкин

---
+++ API_Леготкин/Context/UsersContext.cs
@@ -0,0 +1,23 @@
+﻿using API_Леготкин.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API_Леготкин.Context
+{
+    public class UsersContext:DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=root;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
