4d07759b83ca2eeef3a7acef976b5026abb17fdb
4epni4ka <legotkinc2006@gmail.com>
2024-05-29 06:13:36 +0500

    Разработанный контекст для загрузки данных. Леготкин

---
+++ API_Леготкин/Context/TaskContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_Леготкин.Model;
+using System;
+
+namespace API_Леготкин.Context
+{
+    public class TaskContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=root;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
