cb3bf97b0c74f749d5fcc408dfd774369a17d7ab
4epni4ka <legotkinc2006@gmail.com>
2024-05-31 00:45:16 +0500

    Разработанный контроллер авторизации пользователя. Леготкин

---
+++ API_Леготкин/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API_Леготкин.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using API_Леготкин.Model;
+using System.Linq;
+
+namespace API_Леготкин.Controllers
+{
+    [Route("api/TasksController")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    public class UsersController:Controller
+    {
+        [Route("SingIn")]
+        [HttpGet]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(500)]
+        public ActionResult SingIn([FromForm] string Login, [FromForm] string Password)
+        {
+            if (Login != null || Password == null)
+                return StatusCode(403);
+            try
+            {
+                Users User = new UsersContext().Users.Where(x => x.Login == Login && x.Password == Password).First();
+
+                return Json(User);
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
